f3e45a7c0fbeae8aea5de2eba59320ffe560bbea
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-06 13:05:18 +0800

    Install and configure shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="w-full h-screen grid place-items-center">
       <section className="text-center">
-        <h1 className="text-4xl font-bold text-sky-500">Hyrivo</h1>
+        <h1 className="text-4xl font-semibold text-primary">Hyrivo</h1>
         <h2 className="text-lg">My Job Application Tracker</h2>
       </section>
     </main>
