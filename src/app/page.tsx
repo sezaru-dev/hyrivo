ab73f643e873074523279bdaa1420186fb1d3258
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-06 15:32:45 +0800

    Create painpoints-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CTAFooter from "@/components/sections/CTAFooter";
 import Features from "@/components/sections/Features";
 import Footer from "@/components/sections/Footer";
+import PainPoints from "@/components/sections/PainPoints";
 import Image from "next/image";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <h1 className="text-4xl font-semibold text-primary">Hyrivo</h1>
         <h2 className="text-lg">My Job Application Tracker</h2>
       </section>
+      <PainPoints/>
       <Features/>
       <CTAFooter/>
       <Footer/>
