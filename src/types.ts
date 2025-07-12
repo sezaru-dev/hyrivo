e471a3f3a19446408226e3bb5008bf19167d2265
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-12 23:42:01 +0800

    Add types.ts

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type JobStatus = 'Applied' | 'Interview' | 'Offer' | 'Hired' | 'Rejected' | 'Inactive';
+
+export type JobType =
+  | 'Full-Time Remote'
+  | 'On-Site Full-Time'
+  | 'Hybrid'
+  | 'Contract - 6 months'
+  | 'Internship'
+  | 'Remote Contract'
+  | 'Remote Full-Time'
+  | 'Hybrid Remote';
+
+export type JobApplicationType = {
+  _id: string;
+  companyName: string;
+  jobTitle: string;
+  appliedDate: string;
+  status: JobStatus;
+  interviewDate: string | null;
+  followUp: string;
+  salary: string;
+  jobType: JobType;
+};
