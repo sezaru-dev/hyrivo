263419515cff69581373272c88f7efba8e15166d
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-15 23:11:28 +0800

    feat: add NewApplicationModal component

---
+++ src/components/custom/modals/NewApplicationModal.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import { InputForm } from "../forms/NewApplicationForm";
+
+export default function NewApplicationModal() {
+  const [open, setOpen] = useState(false);
+  const onSubmit = () => {
+    setOpen(false);
+    // Add your mutation logic here
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button className='bg-sidebar-primary text-sidebar-primary-foreground hover:bg-sidebar-primary/90'>
+          <CirclePlus/>
+          New Application
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-xl max-h-[90vh] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>New Job Application</DialogTitle>
+        </DialogHeader>
+        {/* form here */}
+        <InputForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
