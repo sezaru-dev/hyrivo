5d3cbb4a690c4587b84074a0576c64f5acbcc54f
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-17 15:29:57 +0800

    Update global.css and add brand-blue in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
+        "brand-blue": "hsl(var(--brand-blue))",
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
