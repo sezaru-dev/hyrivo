c77b0bdaa193b93645517a92a61826d4d1559346
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-19 15:48:08 +0800

    Create handleRemarksInputSubmit for remarks form submit handler

--- src/lib/form/actions/input-submit.tsx
+++ src/lib/form/actions/input-submit.tsx
@@ -1,8 +1,18 @@
 
 import { toast } from "sonner"
-import { InputFormValues } from "../validations/input-schema"
+import { InputFormValues, RemarksFormValues } from "../validations/input-schema"
 
 export function handleInputSubmit(data: InputFormValues) {
+  toast("You submitted the following values", {
+    description: (
+      <pre className="mt-2 w-[320px] rounded-md bg-neutral-950 p-4">
+        <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    ),
+  })
+}
+
+export function handleRemarksInputSubmit(data: RemarksFormValues) {
   toast("You submitted the following values", {
     description: (
       <pre className="mt-2 w-[320px] rounded-md bg-neutral-950 p-4">
