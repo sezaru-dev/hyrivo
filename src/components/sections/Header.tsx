ce4b7f4bd053b4c08e80e38e8ee3b32999b568ac
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-24 16:17:16 +0800

    Adjust Header component position and styles

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,21 +2,19 @@ import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
 import { ThemeToggle } from '../custom/toggles/ThemeToggle'
+import { LoginDialog } from '../custom/modals/LoginDialog'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-center px-6 py-3 sticky top-4 z-50">
-      <div className="flex items-center justify-between w-full max-w-7xl px-6 py-3 rounded-2xl border border-slate-200 dark:border-slate-800 bg-white/80 dark:bg-slate-950/80 backdrop-blur shadow-sm">
+    <header className="fixed top-8 z-50 w-full flex items-center justify-center">
+      <div className="flex items-center justify-between w-full max-w-6xl mx-4 px-6 py-3 rounded-2xl border-border dark:border-slate-800 bg-muted-foreground/10 backdrop-blur-sm shadow-sm">
         <div className="text-xl font-bold">Hyrivo</div>
         <div className="flex items-center gap-4">
           <ThemeToggle/>
           <nav className="space-x-2">
-            <Link href="/login">
-              <Button variant="ghost">Login</Button>
-            </Link>
-            <Link href="/signup">
-              <Button>Sign Up</Button>
-            </Link>
+            <LoginDialog>
+              <Button className='hover:!bg-blue-700 dark:hover:!bg-blue-800 bg-blue-600 dark:bg-blue-700 text-white'>Login</Button>
+            </LoginDialog>
           </nav>
         </div>
       </div>
@@ -25,3 +23,5 @@ const Header = () => {
 }
 
 export default Header
+
+/* flex items-center justify-center px-6 py-3 */
