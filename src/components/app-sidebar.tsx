124a0ef8b8e606892d56fdce0c15502d6938e8cb
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-25 23:13:28 +0800

    Remove default user data and the assigned props to NavUser component

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -28,11 +28,6 @@ import {
 
 // This is sample data.
 const data = {
-  user: {
-    name: "Sezaru",
-    email: "sezaru@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
   teams: [
     {
       name: "Hyrivo",
@@ -134,7 +129,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
