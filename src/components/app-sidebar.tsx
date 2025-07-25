ad9a9cd391e7816669cdf38c5915c6574ca7c647
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-26 01:19:12 +0800

    Remove user default data

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -28,13 +28,6 @@ import {
 
 // This is sample data.
 const data = {
-  teams: [
-    {
-      name: "Hyrivo",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
-    },
-  ],
   navMain: [
     {
       title: "Applications",
@@ -123,7 +116,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
-        <TeamSwitcher teams={data.teams} />
+        <TeamSwitcher />
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
