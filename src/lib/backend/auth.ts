19498d993fa8f1a4fcb01caf185022b8fb0cf1be
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-25 22:57:49 +0800

    Create auth.ts which contains authOptions

---
+++ src/lib/backend/auth.ts
@@ -0,0 +1,14 @@
+// /src/lib/auth.ts
+import GitHub from "next-auth/providers/github"
+import { NextAuthOptions } from "next-auth"
+import { env } from "@/utils/env"
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHub({
+      clientId: env.GITHUB_ID!,
+      clientSecret: env.GITHUB_SECRET!,
+    }),
+  ],
+  secret: env.NEXTAUTH_SECRET,
+}
