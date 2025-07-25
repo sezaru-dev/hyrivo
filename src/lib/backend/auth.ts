8720e9e5c2ec52e849ba9237fbbe466fc787c985
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-25 23:47:40 +0800

    Change env. to process.env.

--- src/lib/backend/auth.ts
+++ src/lib/backend/auth.ts
@@ -1,4 +1,4 @@
-import GitHub from "next-auth/providers/github"
+/* import GitHub from "next-auth/providers/github"
 import { NextAuthOptions } from "next-auth"
 import { env } from "@/utils/env"
 
@@ -11,3 +11,19 @@ export const authOptions: NextAuthOptions = {
   ],
   secret: env.NEXTAUTH_SECRET,
 }
+ */
+
+import { type NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/", // optional: redirect here when not signed in
+  },
+};
