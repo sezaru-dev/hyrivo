73ad156f66cf3da30d08eb3918cd361583a86c8a
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-27 01:45:45 +0800

    Update env schema to include MONGO_URI and MONGO_DB

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,6 +7,8 @@ export const env = createEnv({
     GITHUB_SECRET: z.string(),
     NEXTAUTH_SECRET: z.string().min(1),
     NEXTAUTH_URL: z.string().url(),
+    MONGO_URI: z.string().url(),
+    MONGO_DB: z.string(),
   },
 
   runtimeEnv: {
@@ -14,5 +16,7 @@ export const env = createEnv({
     GITHUB_SECRET: process.env.GITHUB_SECRET,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    MONGO_URI: process.env.MONGO_URI,
+    MONGO_DB: process.env.MONGO_DB,
   },
 });
