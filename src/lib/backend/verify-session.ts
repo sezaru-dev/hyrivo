f74df2b40ff048c1082cc28bfdd60f1d0be8129d
sezaru-dev <cezarbaguhin20@gmail.com>
2025-07-28 21:02:09 +0800

    Update verifySession return type to NextResponse | Session

--- src/lib/backend/verify-session.ts
+++ src/lib/backend/verify-session.ts
@@ -2,8 +2,9 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "./auth"
 import { NextResponse } from "next/server"
+import type { Session } from "next-auth"
 
-export async function verifySession() {
+export async function verifySession(): Promise<NextResponse | Session> {
   const session = await getServerSession(authOptions)
 
   if (!session || !session.user?.email) {
