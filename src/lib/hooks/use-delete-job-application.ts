7dbc7f2fde6a54db699135f98a4da64725d9e379
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-01 20:53:40 +0800

    Use toastPromise component to show api response message

--- src/lib/hooks/use-delete-job-application.ts
+++ src/lib/hooks/use-delete-job-application.ts
@@ -1,11 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteJobApplication } from "@/lib/queries/job-applications";
+import { toastPromise } from "@/components/custom/toastPromise";
 
 export function useDeleteJobApplication() {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (id: string) => deleteJobApplication(id),
+    mutationFn: (id: string) => toastPromise(() => deleteJobApplication(id), {
+        loading: "Deleting...",
+        success: "Deleted successfully!",
+        error: "Failed to delete.",
+      }),
+     
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["job-applications"] });
     },
