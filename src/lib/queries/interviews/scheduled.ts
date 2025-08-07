f84667ddddb6deecc0e3047c165d0dd667cfc725
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-07 10:28:44 +0800

    feat(api): implement GET and PATCH utilities for scheduled interviews

---
+++ src/lib/queries/interviews/scheduled.ts
@@ -0,0 +1,16 @@
+import { JobApplicationType } from "@/types";
+import { fetcher } from "../fetcher";
+
+export const getScheduledInterviews = () =>
+  fetcher("/api/interviews/scheduled", {
+    method: "GET",
+  });
+  
+export const updateScheduledInterviewNote = (id: string, data: Pick<JobApplicationType, "interviewNote">) =>
+  fetcher(`/api/interviews/scheduled/${id}/notes`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
