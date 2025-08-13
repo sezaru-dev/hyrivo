dc6f24204fc1de74e2ea6d9205f34694e2416b25
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-14 06:17:54 +0800

    feat(hooks): create useCompletedInterviewStats for fetching completed interview stats

---
+++ src/lib/hooks/interviews/completed/use-completed-interview-stats.ts
@@ -0,0 +1,15 @@
+import { fetchCompletedInterviewsStats } from '@/lib/queries/interviews/completed'
+import { useQuery } from '@tanstack/react-query'
+
+
+const useCompletedInterviewStats = () => {
+  return useQuery({
+    queryKey: ["completed-interview-stats"],
+    queryFn: fetchCompletedInterviewsStats,
+    staleTime: 1000 * 60 * 5,         // Consider fresh for 5 mins
+    refetchOnMount: true,            // Refetch in background if data is stale
+    refetchOnWindowFocus: true,      // Useful if user switches tab
+    refetchOnReconnect: true, 
+  })
+}
+export default useCompletedInterviewStats
