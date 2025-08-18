ae50dbd722c5efc5f92962ab630c1102ae96f507
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-18 14:26:50 +0800

    feat(api): create a Get endpoint to get applications with status hired

---
+++ src/app/api/job-applications/hired/route.ts
@@ -0,0 +1,29 @@
+import { connectToDB } from "@/lib/backend/db"
+import { verifySession } from "@/lib/backend/verify-session"
+import JobApplication from "@/models/job-application-model"
+import { NextRequest, NextResponse } from "next/server"
+
+export const GET = async (req: NextRequest) => {
+  const session = await verifySession()
+  if (session instanceof NextResponse) return session
+
+  const userEmail = session.user.email
+
+  await connectToDB()
+
+  try {
+    const applications = await JobApplication.find({ userEmail, status: "hired", interviewStatus: "completed" })
+    if (!applications) {
+      return NextResponse.json({ error: "Job application not found or unauthorized" }, { status: 404 })
+    }
+    return Response.json(applications, { status: 200 });
+  } catch (error) {
+    return Response.json(
+      { message: "Failed to fetch applications" },
+      { status: 500 }
+    );
+  }
+
+
+}
+
