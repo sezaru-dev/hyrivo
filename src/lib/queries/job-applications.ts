057008fd489bf36a525d1bbf59c8a77cd1d12aaa
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-18 15:10:24 +0800

    feat(api): add a patch endpoint to update application status

--- src/lib/queries/job-applications.ts
+++ src/lib/queries/job-applications.ts
@@ -18,6 +18,15 @@ export const createJobApplication = (data: InputFormValues) =>
   });
   
 export const deleteJobApplication = (id: string) =>
-  fetcher(`/api/job-applications/${id}`, {
+  fetcher(`/api/job-applications/${id}/delete`, {
     method: "DELETE",
   });
+
+  export const updateApplicationStatus = (id: string, status: 'applied' | 'interview' | 'offered' | 'hired' | 'rejected') =>
+  fetcher(`/api/job-applications/${id}/status`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ status }),
+});
