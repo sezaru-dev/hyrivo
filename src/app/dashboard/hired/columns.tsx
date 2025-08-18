07c318b5778ef7247984f2e13e26b465e4363272
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-18 14:32:41 +0800

    feat(ui-hired-dashboard-panel): create a hired dashboard panel and its contents

---
+++ src/app/dashboard/hired/columns.tsx
@@ -0,0 +1,123 @@
+"use client"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { JobApplicationType } from "@/types"
+import { format } from "date-fns"
+import { useDropdownMenuStore } from "@/stores/features/dropdownMenuStore"
+import { Badge } from "@/components/ui/badge"
+import { DeleteApplicationDialog } from "@/components/custom/alert-dialogs/DeleteActionDialog"
+
+export const columns: ColumnDef<JobApplicationType>[] = [
+  {
+    accessorKey: "companyName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="-ml-4"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Company
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: "jobTitle",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="-ml-4"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Job Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+  },
+  {
+    accessorKey: "appliedDate",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        className="-ml-4"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Applied Date
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    
+    cell: ({ row }) => {
+      const rawValue = row.getValue("appliedDate")
+
+      if (typeof rawValue !== "string" || !rawValue) {
+        return <span className="text-muted-foreground italic">No date</span>
+      }
+
+      const parsedDate = new Date(rawValue)
+      const formatted = isNaN(parsedDate.getTime())
+        ? "Invalid date"
+        : format(parsedDate, "MMM d, yyyy") // Jun 25, 2025
+
+      return <span>{formatted}</span>
+    },
+  },
+{
+    accessorKey: "jobType",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="-ml-4"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Job Type
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => (
+      <div className="w-32">
+        <Badge variant="outline" className="px-1.5 text-muted-foreground">
+          {row.original.jobType}
+        </Badge>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "salary",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          className="-ml-4"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Salary
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => (
+      <span>{new Intl.NumberFormat('en-PH', { style: 'currency', currency: 'PHP', maximumFractionDigits: 0 }).format(row.original.salary)}</span>
+
+    ),
+  },
+
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const jobApplication = row.original
+      const { setOpenDropdownId } = useDropdownMenuStore()
+ 
+      return (
+        <DeleteApplicationDialog id={jobApplication._id} onAction={() => setOpenDropdownId(null)} withTooltip/>
+      )
+    },
+  },
+]
---
+++ src/app/dashboard/hired/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react'
+import { JobApplicationType } from '@/types'
+import { DataTable } from './data-table'
+import { columns } from './columns'
+import { Skeleton } from '@/components/ui/skeleton'
+import useGetAcceptedJobOffers from '@/lib/hooks/use-hired'
+
+const DashboardCompletedInterview = () => {
+  const { 
+    data, 
+    isLoading, 
+    isError, 
+  } = useGetAcceptedJobOffers()
+  return (
+    <main className="flex-1 p-6 md:p-8 space-y-6 mt-8">
+          {/* Page Heading */}
+          <div className="mb-4">
+              <h1 className="text-2xl font-bold tracking-tight">Hired</h1>
+              <p className="text-muted-foreground text-sm">
+                List of all job applications where you’re already hired.
+              </p>
+          </div>
+
+          {isError ? (
+            <div className="p-4 text-red-500 bg-red-50 border border-red-200 rounded">
+              Failed to load interviews. Please try again later.
+            </div>
+          ) : isLoading ? (
+            <div className="space-y-4 py-4">
+              <Skeleton className="h-9 w-full rounded" />
+              <Skeleton className="h-72 w-full rounded" />
+            </div>
+          ) : (
+            <DataTable columns={columns} data={data as JobApplicationType[] ?? []} />
+          )}
+        </main>
+  )
+}
+
+export default DashboardCompletedInterview
