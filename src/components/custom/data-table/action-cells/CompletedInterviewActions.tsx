5e61b018d134bf505e6efacbe0bbb800474edb0e
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-18 14:55:22 +0800

    feat(ui-completed-column-action): create a custom CompletedInterviewActions that contains actions specific to completed interviews dashboard panel

---
+++ src/components/custom/data-table/action-cells/CompletedInterviewActions.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { useDropdownMenuStore } from '@/stores/features/dropdownMenuStore'
+import { JobApplicationType } from '@/types'
+import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button'
+import { MoreHorizontal } from "lucide-react"
+import ActionDialog from '../../modals/ActionDialog'
+import { DeleteApplicationDialog } from '../../alert-dialogs/DeleteActionDialog'
+import { MarkAsOfferedDialog } from '../../alert-dialogs/MarkAsOfferedDialog'
+import { MarkAsRejectedDialog } from '../../alert-dialogs/MarkAsRejectedDialog'
+
+type ThisComponentProps = {
+  jobApplication: JobApplicationType
+}
+
+export const CompletedInterviewActions = ({ jobApplication}: ThisComponentProps) => {
+  const { openDropdownId, setOpenDropdownId } = useDropdownMenuStore()
+  const isOpen = openDropdownId === jobApplication._id
+
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={(open) => setOpenDropdownId(open ? jobApplication._id : null)}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="grid">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <ActionDialog
+            data={jobApplication}
+            title={jobApplication.interviewRemarks? "Edit Remarks" : "Add Remarks"}
+            form="remarks"
+          >
+            <Button variant="ghost" className=" justify-start px-2">
+              Add/Edit Remarks
+            </Button>
+          </ActionDialog>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <MarkAsOfferedDialog id={jobApplication._id} onAction={() => setOpenDropdownId(null)}/>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <MarkAsRejectedDialog id={jobApplication._id} onAction={() => setOpenDropdownId(null)}/>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <DeleteApplicationDialog id={jobApplication._id} onAction={() => setOpenDropdownId(null)}/>
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
