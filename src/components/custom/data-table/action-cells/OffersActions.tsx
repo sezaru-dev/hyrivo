cf975d4aaa978aabbd9e54dd29b1a934918c4db2
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-18 14:56:31 +0800

    feat(ui-offers-column-action): create a custom OffersActions that contains actions specific to offers dashboard panel

---
+++ src/components/custom/data-table/action-cells/OffersActions.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { useDropdownMenuStore } from '@/stores/features/dropdownMenuStore'
+import { JobApplicationType } from '@/types'
+import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button'
+import { MoreHorizontal } from "lucide-react"
+import { DeleteApplicationDialog } from '../../alert-dialogs/DeleteActionDialog'
+import { MarkAsHiredDialog } from '../../alert-dialogs/MarkAsHiredDialog'
+
+type ThisComponentProps = {
+  jobApplication: JobApplicationType
+}
+
+export const OffersActions = ({ jobApplication}: ThisComponentProps) => {
+  const { openDropdownId, setOpenDropdownId } = useDropdownMenuStore()
+  const isOpen = openDropdownId === jobApplication._id
+
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={(open) => setOpenDropdownId(open ? jobApplication._id : null)}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="grid">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <MarkAsHiredDialog id={jobApplication._id} onAction={() => setOpenDropdownId(null)}/>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <DeleteApplicationDialog id={jobApplication._id} onAction={() => setOpenDropdownId(null)}/>
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
