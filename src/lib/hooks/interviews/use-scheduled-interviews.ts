4f70e3f9c2daaf5e0b02fa56f66ad997feb05140
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-19 12:48:17 +0800

    enhance useGetScheduledInterviews hook to handle limit query

--- src/lib/hooks/interviews/use-scheduled-interviews.ts
+++ src/lib/hooks/interviews/use-scheduled-interviews.ts
@@ -1,10 +1,11 @@
 import { getScheduledInterviews } from '@/lib/queries/interviews/scheduled'
+import { JobApplicationType } from '@/types'
 import { useQuery } from '@tanstack/react-query'
 
-const useGetScheduledInterviews = () => {
-  return useQuery({
-    queryKey: ["scheduled-interviews"],
-    queryFn: getScheduledInterviews,
+const useGetScheduledInterviews = (limit?: number) => {
+  return useQuery<JobApplicationType[]>({
+    queryKey: ["scheduled-interviews", limit],
+    queryFn: () => getScheduledInterviews(limit),
     staleTime: 1000 * 60 * 5,         // Consider fresh for 5 mins
     refetchOnMount: true,            // Refetch in background if data is stale
     refetchOnWindowFocus: true,      // Useful if user switches tab
