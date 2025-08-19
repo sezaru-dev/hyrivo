62ae2fa32196f1b666048a98ceacb5cfd37b745a
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-19 18:51:20 +0800

    remove the type check for access, refresh, and base url because i dont need it anymore

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -9,11 +9,6 @@ export const env = createEnv({
     NEXTAUTH_URL: z.string().url(),
     MONGO_URI: z.string().url(),
     MONGO_DB: z.string(),
-    ACCESS_KEY: z.string(),
-    REFRESH_KEY: z.string(),
-  },
-  client: {
-    NEXT_PUBLIC_BASE_URL: z.string().url(),
   },
 
   runtimeEnv: {
@@ -23,9 +18,5 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     MONGO_URI: process.env.MONGO_URI,
     MONGO_DB: process.env.MONGO_DB,
-    ACCESS_KEY: process.env.ACCESS_KEY,
-    REFRESH_KEY: process.env.REFRESH_KEY,
-
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 });
