5294f8f1e505852dabba6d684d56af3806b5dc83
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-19 12:38:20 +0800

    feat(job-applications-api): improve GET api endpoint to handle limit query params

--- src/app/api/job-applications/route.ts
+++ src/app/api/job-applications/route.ts
@@ -4,28 +4,37 @@ import { verifySession } from "@/lib/backend/verify-session";
 import JobApplication from "@/models/job-application-model";
 import { NextRequest, NextResponse } from "next/server"
 
-export async function GET() {
+export async function GET(req: Request) {
   const session = await verifySession()
   if (session instanceof NextResponse) return session
 
   try {
-    await connectToDB();
+    await connectToDB()
 
     const userEmail = session.user.email
-  
-     const applications = await JobApplication.find({ userEmail })
-      .sort({ appliedDate: -1 })
-      .lean()
 
-    return Response.json(applications, { status: 200 });
+    const { searchParams } = new URL(req.url)
+    const limitParam = searchParams.get("limit")
+    const limit = limitParam ? Number(limitParam) : null
+
+    let query = JobApplication.find({ userEmail }).sort({ appliedDate: -1 })
+
+    if (limit !== null && !isNaN(limit)) {
+      query = query.limit(limit)
+    }
+
+    const applications = await query.lean()
+
+    return Response.json(applications, { status: 200 })
   } catch (error) {
-    console.error("[GET_JOB_APPLICATIONS_ERROR]", error);
+    console.error("[GET_JOB_APPLICATIONS_ERROR]", error)
     return Response.json(
-      { message: "Failed to fetch users" },
+      { message: "Failed to fetch applications" },
       { status: 500 }
-    );
+    )
   }
 }
+
 
 
 
