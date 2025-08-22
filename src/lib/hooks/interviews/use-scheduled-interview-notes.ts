3b799e83863387ff79b63cf083dff567bb71be71
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-22 10:55:26 +0800

    Add toastPromise to mutationFn with toast status message

--- src/lib/hooks/interviews/use-scheduled-interview-notes.ts
+++ src/lib/hooks/interviews/use-scheduled-interview-notes.ts
@@ -1,11 +1,17 @@
 import { updateScheduledInterviewNote } from '@/lib/queries/interviews/scheduled'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { JobApplicationType } from '@/types'
+import { toastPromise } from '@/components/custom/toastPromise';
 
 const useUpdateScheduledInterviewNotes = () => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({id,data}: {id: string, data: Pick<JobApplicationType, 'interviewNote'>}) => updateScheduledInterviewNote(id, data),
+    mutationFn: ({id,data}: {id: string, data: Pick<JobApplicationType, 'interviewNote'>}) => toastPromise(() => updateScheduledInterviewNote(id, data),
+    {
+      loading: "Saving note...",
+      success: "Your note has been saveds!",
+      error: "Failed to save note.",
+    }),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["scheduled-interviews"] });
       queryClient.invalidateQueries({ queryKey: ["scheduled-interview-stats"] });
