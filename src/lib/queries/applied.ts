9143c7f4945adff058461fd9a7d0cc2d5e9a6663
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-22 16:40:01 +0800

    delete createJobApplicationTest

--- src/lib/queries/applied.ts
+++ src/lib/queries/applied.ts
@@ -1,18 +1,10 @@
 import { JobApplicationType } from "@/types";
-/* import { NewApplicationFormValues } from "../form/validations/input-schema"; */
 import { fetcher } from "./fetcher";
-
 
 export const fetchJobApplicationsStatusApplied = () =>
   fetcher<JobApplicationType[]>("/api/job-applications/applied", {
     method: "GET",
   });
-
-/* export const createJobApplicationtest = (data: NewApplicationFormValues) =>
-  fetcher("/api/job-applications/applied", {
-    method: "POST",
-    body: JSON.stringify(data),
-  }); */
 
 // PATCH /api/job-applications/applied/:id
   export const setInterviewSchedule = (id: string, data: Pick<JobApplicationType, "interviewAt" | "interviewMethod" | "interviewNote">) => {
