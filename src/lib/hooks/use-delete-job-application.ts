b2c029caa0151d31f063d513f1a62e88ef0574e2
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-22 11:02:41 +0800

    Add invalidateQueries for job-applications-applied and job-applications-stats

--- src/lib/hooks/use-delete-job-application.ts
+++ src/lib/hooks/use-delete-job-application.ts
@@ -14,6 +14,8 @@ export function useDeleteJobApplication() {
      
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["job-applications"] });
+      queryClient.invalidateQueries({ queryKey: ["job-applications-applied"] });
+      queryClient.invalidateQueries({ queryKey: ["job-applications-stats"] });
       queryClient.invalidateQueries({ queryKey: ["scheduled-interviews"] });
       queryClient.invalidateQueries({ queryKey: ["scheduled-interview-stats"] });
     },
