ba6d81ca1e6f0a2b744f2664df433db59b18c543
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-22 11:10:48 +0800

    refactor formschema, handleSubmitNote, and add textarea input  counter

--- src/components/custom/forms/NotesForm.tsx
+++ src/components/custom/forms/NotesForm.tsx
@@ -11,11 +11,9 @@ import {
   FormControl,
   FormMessage,
 } from "@/components/ui/form"
-
 import { Textarea } from "@/components/ui/textarea"
 import { ActionDialogProps } from "../modals/ActionDialog"
 import useUpdateScheduledInterviewNotes from "@/lib/hooks/interviews/use-scheduled-interview-notes"
-import { toast } from "sonner"
 import { JobApplicationType } from "@/types"
 import z from "zod"
 
@@ -24,15 +22,17 @@ type NotesFormProps = Omit<ActionDialogProps, 'children' | 'title' | 'form'> & {
   onSubmit?: () => void
 }
 
+type SubmitHandlerParams = {
+  values: z.infer<typeof FormSchema>
+}
+
 const FormSchema = z.object({
   interviewNote: z
-    .string()
-    .min(10, {
-      message: "Notes must be at least 10 characters.",
-    })
-    .max(200, {
-      message: "Notes must not be longer than 200 characters.",
-    }),
+  .string()
+  .optional()
+  .refine((val) => !val || (val.length >= 10 && val.length <= 200), {
+    message: "Notes must be between 10 and 200 characters if provided",
+  }),
 })
 
 export default function NotesForm({data, onSubmit}: NotesFormProps) {
@@ -43,63 +43,70 @@ export default function NotesForm({data, onSubmit}: NotesFormProps) {
     },
   })
 
-  const mutation = useUpdateScheduledInterviewNotes()
+  const {mutateAsync, isPending, isSuccess} = useUpdateScheduledInterviewNotes()
 
-  
+  const handleSubmitNote = async ({values}:SubmitHandlerParams) => {
+    try {
+      await mutateAsync({
+        id: data._id,
+        data: { interviewNote: values.interviewNote ?? null },
+      })
+
+      // only run if mutation succeeds
+      form.reset()
+      onSubmit?.()
+    } catch (error) {
+      console.error("Reschedule failed:", error)
+    }
+  }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(
-          async (values) => {
-             toast.promise(
-              mutation.mutateAsync({
-                id: data._id,
-                data: { interviewNote: values.interviewNote },
-              }),
-              {
-                loading: <span>Saving note...</span>,
-                success: () => {
-                  form.reset()
-                  onSubmit?.()
-                  return <span className="text-green-500">Note saved successfully</span>
-                },
-                error: (err: unknown) => {
-                  if (err instanceof Error) {
-                    return <span className="text-red-500">{err.message}</span>
-                  }
-                  return <span className="text-red-500">Something went wrong</span>
-                }
-              }
-            )
-          },
-          (errors) => {
-            console.log("Validation errors:", errors)
-          }
+        (values: z.infer<typeof FormSchema>) =>
+          handleSubmitNote({
+          values,
+        })
         )} className="space-y-6">
           {/* Company */}
           <FormField
             control={form.control}
             name="interviewNote"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Notes</FormLabel>
-                <FormControl>
-                  <Textarea
-                    defaultValue={data?.interviewNote ?? ""}
-                    placeholder={!data?.interviewNote ? "Add any notes or details about the scheduled interview..." : ""}
-                    className="resize-none"
-                    rows={4}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+            render={({ field }) => {
+              const currentLength = field.value?.length ?? 0
+              const maxLength = 200
+              return (
+                <FormItem>
+                  <FormLabel>Notes</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder={!data?.interviewNote ? "Add any notes or details about the scheduled interview..." : ""}
+                      className="resize-none"
+                      rows={4}
+                      {...field}
+                    />
+                  </FormControl>
+                  {/* Error + Counter row */}
+                    <div className="grid grid-cols-12 mt-1 text-xs">
+                      <FormMessage className="col-span-11"/>
+                        <span
+                          className={`col-start-12 col-span-1 ${
+                            currentLength > maxLength - 20 ? "text-red-500" : "text-muted-foreground"
+                          }`}
+                        >
+                          {currentLength}/{maxLength}
+                        </span>
+                    </div>
+                </FormItem>
+              )
+            }}
           />
 
       {/* Submit button */}
       <div className="flex justify-end pt-4">
-        <Button type="submit" className='bg-brand-blue text-sidebar-primary-foreground hover:bg-brand-blue/80'>Submit</Button>
+        <Button type="submit" className='bg-brand-blue text-sidebar-primary-foreground hover:bg-brand-blue/80' disabled={isPending || isSuccess}>
+          Save
+        </Button>
       </div>
 </form>
 
