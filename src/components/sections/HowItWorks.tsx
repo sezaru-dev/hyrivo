d8957425143e5a9fceeca7447cb6b6b417c35b42
sezaru-dev <cezarbaguhin20@gmail.com>
2025-08-22 22:44:13 +0800

    add howitworks id and enhance styles

--- src/components/sections/HowItWorks.tsx
+++ src/components/sections/HowItWorks.tsx
@@ -3,7 +3,7 @@ import { HowItWorksSteps } from "@/constant/constant-data";
 
 export default function HowItWorks() {
   return (
-    <section className="py-20 px-4 max-w-6xl mx-auto">
+    <section className="py-36 px-4 max-w-6xl mx-auto scroll-pt-20" id="howitworks">
       <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
         How It Works
       </h2>
@@ -11,7 +11,7 @@ export default function HowItWorks() {
       <div className="relative flex flex-col md:flex-row items-center justify-between gap-8 md:gap-4">
         {HowItWorksSteps.map((step, index) => (
           <div key={index} className="flex flex-col items-center text-center max-w-sm">
-            <div className="relative z-10 p-4 bg-blue-100 dark:bg-blue-600 rounded-full mb-4">
+            <div className="relative z-10 p-4 bg-blue-100 dark:bg-blue-600 rounded-full mb-4 text-red-400">
               {step.icon}
             </div>
             <h3 className="text-lg font-semibold mb-1">{step.title}</h3>
